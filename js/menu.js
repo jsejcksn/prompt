7361978676ab025823bd9570b4b0fc1d7911e8ac
Jesse Jackson <jsejcksn@users.noreply.github.com>
2016-10-28 15:09:11 -0500

    Add some more fun logic

--- js/menu.js
+++ js/menu.js
@@ -1,44 +1,104 @@
 (function() {
   'use strict';
+  const p = prompt;
 
   const stdin = document.getElementById('stdin');
-  let lastKey;
+  const logicState = {
+    lastKey: '',
+    almostNumber: 0
+  };
 
-  prompt.scrollBottom();
-  setTimeout(() => {
-    prompt.prompt([
-      'Hello—I\'m Prompt.'
-    ]);
-    setTimeout(() => {
-      prompt.prompt([
-        'I don\'t do anything yet, so you can send me messages and I won\'t reply to you!'
-      ]);
-    }, 1500);
-  }, 1000);
+  p.scrollBottom();
+  timedPrompt([
+    'Hello—I\'m Prompt.'
+  ], 1000);
+  timedPrompt([
+    'I don\'t do anything yet, so you can send me messages and I won\'t reply to you!'
+  ], 2500);
+
+  function almostNumber (str) {
+    const arr = str.split('');
+    let verdict = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (!isNaN(arr[i])) {
+        verdict++;
+      }
+    }
+    return verdict === 0 ? false : true;
+  }
 
   function finalMsg () {
     const e = event;
     const code = e.keyCode || e.which;
-    if (lastKey !== 13) {
+    if (logicState.lastKey !== 13) {
       if (code === 13) { // Enter keycode
         setTimeout(() => {
-          if (prompt.history.length === 1) {
-            setTimeout(() => {
-              prompt.prompt([
-                'Actually, I do keep scrolling when you return things, but that\'s it.'
+          if (p.history.length === 1) {
+            timedPrompt([
+              'Actually, I do keep scrolling when you return things, but that\'s it.'
+            ]);
+          } else if (p.history.length === 2) {
+            timedPrompt([
+              'Have fun typing to no one.'
+            ]);
+          } else if (p.history.length === 5) {
+            timedPrompt([
+              'You seem to be determined. I\'ll show you one more thing I can do—I\'ll tell you if what you enter is a number or not a number.'
+            ]);
+            timedPrompt([
+              'And I won\'t do anything else.'
+            ], 2400);
+          } else if (p.history.length === 106) {
+            timedPrompt([
+              'That\'s 100 number tests! 🌟🎉👏'
+            ]);
+            if (isNaN(p.history[p.history.length - 1])) {
+              if (logicState.almostNumber === 0 && almostNumber(p.history[p.history.length - 1])) {
+                timedPrompt([
+                  'Clever, but you can\'t fool me. Not a number.'
+                ]);
+                logicState.almostNumber++;
+              } else {
+                timedPrompt([
+                  'But that\'s still not a number.'
+                ]);
+              }
+            } else {
+              timedPrompt([
+                'But that\'s still just a number.'
               ]);
-            }, 600);
-          } else if (prompt.history.length === 2) {
-            setTimeout(() => {
-              prompt.prompt([
-                'Have fun typing to no one.'
+            }
+          } else if (p.history.length >= 6) {
+            if (isNaN(p.history[p.history.length - 1])) {
+              if (logicState.almostNumber === 0 && almostNumber(p.history[p.history.length - 1])) {
+                timedPrompt([
+                  'Clever, but you can\'t fool me. Not a number.'
+                ]);
+                logicState.almostNumber++;
+              } else {
+                timedPrompt([
+                  'Not a number'
+                ]);
+              }
+            } else {
+              timedPrompt([
+                'Number'
               ]);
-            }, 600);
+            }
           }
-        }, 50);
+        }, 20);
       }
     }
-    lastKey = code;
+    logicState.lastKey = code;
+  }
+
+  function timedPrompt(msg, delay) {
+    const cond = () => {
+      return delay ? delay : 600;
+    };
+    setTimeout(() => {
+      p.prompt(msg);
+    }, cond());
   }
 
   stdin.addEventListener('keyup', finalMsg );
